6b21ab7b04a815422a37393a6bf98946e4e458eb
suruy <turucham45@gmail.com>
2019-07-05 17:55:02 +0900

    db and foreach list

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-var cariSchema = new Schema({
-  title: String,
-  message: String,
-  date: 
-  {
-    type: Date,
-    default: Date.now
-  }
-});
-
-var Cari = mongoose.model('caris',cariSchema);
+var moment = require('moment');
+var Cari = require('../mongooseConnection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Cari.find({}).sort('-date').exec( function(err,docs){
+    console.log(docs);
+    if(err) console.log(err);
+    res.render('index', { 'docs': docs });
+  });
 });
 
 /*post 受け取り*/
 router.post('/', function(req, res, next) {
   var cari = new Cari(req.body);
-  var body = req.body;
+  var body = req.body
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
   console.log(body);
+  console.log(createdAt);
+  
   cari.save(function (err) {
     if(err) console.log(err);
+    res.redirect('/');
   })
 });
 
